2a029228931131099296ba906358f88deab7fffa
Carlos Eduardo Lima <carloseduardolima.dev@gmail.com>
2023-08-06 17:42:04 -0300

    feat: edited contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -10,6 +10,8 @@ const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
+    if(!form.current) return
+
     emailjs
       .sendForm(
         "service_ssiy27b",
@@ -21,7 +23,7 @@ const Contact = () => {
         (result) => {
           console.log(result.text);
           setDone(true);
-          form.reset();
+          form.current?.reset();
         },
         (error) => {
           console.log(error.text);
@@ -35,8 +37,8 @@ const Contact = () => {
       <div className="w-left">
         <div className="awesome">
           {/* darkMode */}
-          <span style={{color: darkMode?'white': ''}}>Get in Touch</span>
-          <span>Contact me</span>
+          <span style={{color: darkMode?'white': ''}}>Quer falar comigo?</span>
+          <span>Envie uma mensagem!</span>
           <div
             className="blur s-blur1"
             style={{ background: "#ABF1FF94" }}
@@ -46,11 +48,11 @@ const Contact = () => {
       {/* right side form */}
       <div className="c-right">
         <form ref={form} onSubmit={sendEmail}>
-          <input type="text" name="user_name" className="user"  placeholder="Name"/>
+          <input type="text" name="user_name" className="user"  placeholder="Nome"/>
           <input type="email" name="user_email" className="user" placeholder="Email"/>
-          <textarea name="message" className="user" placeholder="Message"/>
-          <input type="submit" value="Send" className="button"/>
-          <span>{done && "Thanks for Contacting me"}</span>
+          <textarea name="message" className="user" placeholder="Mensagem"/>
+          <input type="submit" value="Enviar" className="button"/>
+          <span>{done && "Obrigado pela mensagem. Em breve te respondo!!!"}</span>
           <div
             className="blur c-blur1"
             style={{ background: "var(--purple)" }}
